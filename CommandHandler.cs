5e1c4c53899ab208d75826c62abcc699f505b834
Microjack5 <pelijah25@yahoo.com>
2022-05-18 16:01:10 -0500

    Merge context + slash commands to one object

--- CommandHandler.cs
+++ CommandHandler.cs
@@ -7,9 +7,111 @@ using Microsoft.Extensions.DependencyInjection;
 using SocialLinker.Config;
 using SocialLinker.Core.LevelSystem;
 using Fergun.Interactive;
+using System.Collections.Generic;
+using System.Linq;
+using Discord;
+using Discord.Interactions;
+using Newtonsoft.Json;
+using Discord.Net;
 
 namespace SocialLinker
 {
+    public class SocialLinkerCommand
+    {
+        public string CommandType { get; set; }
+        public string CommandName { get; set; }
+        public SocketUser User { get; set; }
+        public ISocketMessageChannel Channel { get; set; }
+        public SocketUser MentionedUser { get; set; }
+        public string Content { get; set; }
+        public IReadOnlyCollection<Attachment> Attachments { get; set; }
+        public SocketUserMessage Message { get; set; }
+        public SocketSlashCommand SlashCommand { get; set; }
+    }
+
+    public class CommandConverter : InteractionModuleBase<SocketInteractionContext>
+    {
+        public static SocialLinkerCommand SlashCommandConverter(SocketSlashCommand command)
+        {
+            SocketUser data_to_mentioneduser;
+            string data_to_string;
+
+            if ((command.Data != null)) // Maker, help, settings, shop, status
+            {
+                data_to_mentioneduser = (SocketUser)command.Data.Options.First().Value;
+                data_to_string = null;
+            }
+            else
+            {
+                data_to_mentioneduser = null;
+                data_to_string = command.Data.Options.ToString();
+            }
+
+            SocialLinkerCommand slash_to_command = new SocialLinkerCommand
+            {
+                CommandType = "Slash",
+                CommandName = command.CommandName,
+                User = command.User,
+                Channel = command.Channel,
+                MentionedUser = data_to_mentioneduser,
+                Content = data_to_string,
+                Message = null,
+                SlashCommand = command
+            };
+
+            return slash_to_command;
+        }
+
+        public static SocialLinkerCommand ContextCommandConverter(SocketMessage message)
+        {
+            List<string> input_substring;
+
+            char[] delimiterChars = { ' ' };
+
+            input_substring = message.Content.Split(delimiterChars).ToList();
+
+            int prefix_length = $"{BotConfig.bot.cmdPrefix}".Length;
+
+            string parsed_command_name = input_substring[0].Substring(prefix_length);
+
+            if (input_substring.Count > 1)
+            {
+                input_substring.RemoveAt(0);
+            }
+
+            string parsed_content = String_List_To_String(input_substring);
+
+            SocialLinkerCommand context_to_command = new SocialLinkerCommand
+            {
+                CommandType = "Context",
+                CommandName = parsed_command_name,
+                User = message.Author,
+                Channel = message.Channel,
+                MentionedUser = message.MentionedUsers.First(),
+                Content = parsed_content,
+                Attachments = message.Attachments,
+                Message = (SocketUserMessage)message
+            };
+
+            return context_to_command;
+        }
+
+        public static string String_List_To_String(List<string> input_list)
+        {
+            // Create an empty string variable.
+            string output_string = "";
+
+            // Iterate through each index of the list and add it to the string variable.
+            for (int i = 0; i < input_list.Count; i++)
+            {
+                output_string += input_list[i];
+            }
+
+            // Return the string variable.
+            return output_string;
+        }
+    }
+
     class CommandHandler
     {
         DiscordShardedClient _client;
@@ -24,6 +126,16 @@ namespace SocialLinker
             _Services = ConfigureServices();
             await _service.AddModulesAsync(Assembly.GetEntryAssembly(), _Services);
             _client.MessageReceived += HandleCommandAsync;
+
+            _client.ShardReady += Status;
+            /*_client.ShardReady += Shop;
+            _client.ShardReady += Settings;
+            _client.ShardReady += Help;
+            _client.ShardReady += Hug;
+            _client.ShardReady += Pat;
+            _client.ShardReady += Punch;
+            _client.ShardReady += Slap; */
+            await Task.CompletedTask;
 
             _interactionHandler = new Commands.InteractionHandler();
             await _interactionHandler.InitializeAsync(_client);
@@ -64,6 +176,245 @@ namespace SocialLinker
 
             //Leveling up manages the user's time caps, so make sure it comes after AddProficiency and AddDiligence have ran
             Leveling.UserSentMessage(msg);
+        }
+
+        private async Task HandleContextCommandAsync(SocketMessage s)
+        {
+            var msg = s as SocketUserMessage;
+            if (msg == null) return;
+            var context = new ShardedCommandContext(_client, msg);
+            if (context.User.IsBot) return;
+
+            // If the message is a direct message, return immediately.
+            if (msg.Channel.GetType() == typeof(SocketDMChannel))
+            {
+                return;
+            }
+
+            //If the user is in a time out status, do nothing and return
+            if (TimeOut.TimeOutStatus(msg) == "Yes") return;
+
+            int argPos = 0;
+            if (msg.HasStringPrefix(BotConfig.bot.cmdPrefix, ref argPos))
+            {
+                var converted_sl_command = CommandConverter.ContextCommandConverter(msg);
+
+                //Add Proficiency to the user's account whenever a command is successfully used
+                //SocialStats.AddProficiency(msg);
+            }
+
+            //Calculate if the user gains Diligence for this message
+            //SocialStats.AddDiligence(msg);
+
+            //Leveling up manages the user's time caps, so make sure it comes after AddProficiency and AddDiligence have ran
+            //Leveling.UserSentMessage(msg);
+        }
+
+        private async Task CommandIndex(SocialLinkerCommand command)
+        {
+            if (command.CommandType == "Slash")
+            {
+                await command.SlashCommand.RespondAsync(embed: Slash_Command_Successful().Build(), ephemeral: true);
+            }
+
+            switch (command.CommandName)
+            {
+                case "status":
+
+                    break;
+
+                case "shop":
+                    break;
+
+                case "settings":
+                    break;
+
+                case "help":
+                    break;
+
+                case "hug":
+                    break;
+
+                case "pat":
+                    break;
+
+                case "punch":
+                    break;
+
+                case "slap":
+                    break;
+
+                case "maker":
+                    break;
+            }
+        }
+
+        public static async Task Status(DiscordSocketClient client)
+        {
+            ulong guildId = 543226698238394378;
+            //await client.GetGuild(543226698238394378).DeleteApplicationCommandsAsync();
+
+            var guildCommand = new SlashCommandBuilder()
+                .WithName("status")
+                .WithDescription("View your status screen, or specify a user to view theirs.")
+                .AddOption("user", ApplicationCommandOptionType.User, "The user whose status you want to view.", isRequired: false);
+
+            try
+            {
+                await client.Rest.CreateGuildCommand(guildCommand.Build(), guildId);
+            }
+            catch (HttpException exception)
+            {
+                var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+                Console.WriteLine(json);
+            }
+        }
+
+        public static async Task Shop(DiscordSocketClient client)
+        {
+            ulong guildId = 543226698238394378;
+
+            var guildCommand = new SlashCommandBuilder()
+                .WithName("shop")
+                .WithDescription("Browse the décor shop.");
+
+            try
+            {
+                await client.Rest.CreateGuildCommand(guildCommand.Build(), guildId);
+            }
+            catch (HttpException exception)
+            {
+                var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+                Console.WriteLine(json);
+            }
+        }
+
+        public static async Task Settings(DiscordSocketClient client)
+        {
+            ulong guildId = 543226698238394378;
+
+            var guildCommand = new SlashCommandBuilder()
+                .WithName("settings")
+                .WithDescription("Change and customize user settings.");
+
+            try
+            {
+                await client.Rest.CreateGuildCommand(guildCommand.Build(), guildId);
+            }
+            catch (HttpException exception)
+            {
+                var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+                Console.WriteLine(json);
+            }
+        }
+
+        public static async Task Help(DiscordSocketClient client)
+        {
+            ulong guildId = 543226698238394378;
+
+            var guildCommand = new SlashCommandBuilder()
+                .WithName("help")
+                .WithDescription("Learn how to use Social Linker.");
+
+            try
+            {
+                await client.Rest.CreateGuildCommand(guildCommand.Build(), guildId);
+            }
+            catch (HttpException exception)
+            {
+                var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+                Console.WriteLine(json);
+            }
+        }
+
+        public static async Task Hug(DiscordSocketClient client)
+        {
+            ulong guildId = 543226698238394378;
+
+            var guildCommand = new SlashCommandBuilder()
+                .WithName("hug")
+                .WithDescription("Give a user a hug.")
+                .AddOption("user", ApplicationCommandOptionType.User, "The user you want to hug.", isRequired: true);
+
+            try
+            {
+                await client.Rest.CreateGuildCommand(guildCommand.Build(), guildId);
+            }
+            catch (HttpException exception)
+            {
+                var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+                Console.WriteLine(json);
+            }
+        }
+
+        public static async Task Pat(DiscordSocketClient client)
+        {
+            ulong guildId = 543226698238394378;
+
+            var guildCommand = new SlashCommandBuilder()
+                .WithName("pat")
+                .WithDescription("Give a user a pat.")
+                .AddOption("user", ApplicationCommandOptionType.User, "The user you want to pat.", isRequired: true);
+
+            try
+            {
+                await client.Rest.CreateGuildCommand(guildCommand.Build(), guildId);
+            }
+            catch (HttpException exception)
+            {
+                var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+                Console.WriteLine(json);
+            }
+        }
+
+        public static async Task Punch(DiscordSocketClient client)
+        {
+            ulong guildId = 543226698238394378;
+
+            var guildCommand = new SlashCommandBuilder()
+                .WithName("punch")
+                .WithDescription("Give out unbridled violence.")
+                .AddOption("user", ApplicationCommandOptionType.User, "The user you want to punch.", isRequired: true);
+
+            try
+            {
+                await client.Rest.CreateGuildCommand(guildCommand.Build(), guildId);
+            }
+            catch (HttpException exception)
+            {
+                var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+                Console.WriteLine(json);
+            }
+        }
+
+        public static async Task Slap(DiscordSocketClient client)
+        {
+            ulong guildId = 543226698238394378;
+
+            var guildCommand = new SlashCommandBuilder()
+                .WithName("slap")
+                .WithDescription("Hand out a high five. In the face.")
+                .AddOption("user", ApplicationCommandOptionType.User, "The user you want to slap.", isRequired: true);
+
+            try
+            {
+                await client.Rest.CreateGuildCommand(guildCommand.Build(), guildId);
+            }
+            catch (HttpException exception)
+            {
+                var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+                Console.WriteLine(json);
+            }
+        }
+
+        public static EmbedBuilder Slash_Command_Successful()
+        {
+            var embed = new EmbedBuilder();
+
+            embed.WithColor(0, 207, 41);
+            embed.WithDescription("**Slash command successful** :white_check_mark:");
+
+            return embed;
         }
 
         private IServiceProvider ConfigureServices()
--- Commands/Help.cs
+++ Commands/Help.cs
@@ -14,40 +14,40 @@ namespace SocialLinker.Commands
     public class Help : ModuleBase<SocketCommandContext>
     {
         [Command("help", RunMode = RunMode.Async)]
-        public async Task HelpMenu()
+        public async Task HelpMenu(SocialLinkerCommand command)
         {
             // If there is a cooldown session active for the command type "menu", return the method immediately.
-            if (await UserCooldownMethods.IsCooldownActive(Context.Message, "menu") == true)
+            /*if (await UserCooldownMethods.IsCooldownActive(Context.Message, "menu") == true) // COME BACK TO HERE SOON
             {
                 return;
-            }
+            } */
 
             // Get the account information of the command's user.
-            var command_user_account = UserInfoClasses.GetAccount(Context.User);
+            var command_user_account = UserInfoClasses.GetAccount(command.User);
 
             // Check if the user's account has been activated. If not, send them to the initial usage setup menu.
             if (command_user_account.Account_Activated == "No")
             {
-                await First_Use_Content_Filter_Menu.First_Use_Content_Filter_Start((SocketTextChannel)Context.Channel, (SocketGuildUser)Context.User);
+                await First_Use_Content_Filter_Menu.First_Use_Content_Filter_Start((SocketTextChannel)command.Channel, (SocketGuildUser)command.User);
                 return;
             }
 
             // End of initial usage and cooldown checks.
 
             // Create two variables to check if there is a menu list entry with either the current channel ID or current user ID.
-            var channelSearch = Global.MenuIdList.SingleOrDefault(x => x.MenuMessage.Channel.Id == Context.Channel.Id);
-            var userSearch = Global.MenuIdList.SingleOrDefault(x => x.User.Id == Context.User.Id);
+            var channelSearch = Global.MenuIdList.SingleOrDefault(x => x.MenuMessage.Channel.Id == command.Channel.Id);
+            var userSearch = Global.MenuIdList.SingleOrDefault(x => x.User.Id == command.User.Id);
 
             // If the channel entry exists and the user is not the same, send an error message.
-            if (channelSearch != null && channelSearch.User.Id != Context.User.Id)
+            if (channelSearch != null && channelSearch.User.Id != command.User.Id)
             {
                 // Case 1: Search by channel successful, user ID does not match. Create new entry for new user.
                 // Create a new menu in the current channel.
-                await Help_Menu.Help_Start((SocketTextChannel)Context.Channel, (SocketGuildUser)Context.User);
+                await Help_Menu.Help_Start((SocketTextChannel)command.Channel, (SocketGuildUser)command.User);
                 return;
             }
             // Else, if the channel entry exists and the user is the same, assume they want to reset the menu and delete the previous entry.
-            else if (channelSearch != null && channelSearch.User.Id == Context.User.Id)
+            else if (channelSearch != null && channelSearch.User.Id == command.User.Id)
             {
                 // Case 2: Search by channel successful, user ID matches. Resetting menu in same channel.
                 // Attempt deleting the message if it hasn't been deleted by the user yet.
@@ -68,11 +68,11 @@ namespace SocialLinker.Commands
                 Global.MenuIdList.Remove(channelSearch);
 
                 // Create a new menu in the current channel.
-                await Help_Menu.Help_Start((SocketTextChannel)Context.Channel, (SocketGuildUser)Context.User);
+                await Help_Menu.Help_Start((SocketTextChannel)command.Channel, (SocketGuildUser)command.User);
                 return;
             }
             // Else, if an entry exists where the user is found but they're in a different channel now, delete previous entry and reset the menu.
-            else if (userSearch != null && userSearch.MenuMessage.Channel.Id != Context.Channel.Id)
+            else if (userSearch != null && userSearch.MenuMessage.Channel.Id != command.Channel.Id)
             {
                 // Case 3: Search by user successful, channel ID does not match. Resetting menu in new channel.
                 // Attempt deleting the message if it hasn't been deleted by the user yet.
@@ -93,7 +93,7 @@ namespace SocialLinker.Commands
                 Global.MenuIdList.Remove(userSearch);
 
                 // Create a new menu in the current channel.
-                await Help_Menu.Help_Start((SocketTextChannel)Context.Channel, (SocketGuildUser)Context.User);
+                await Help_Menu.Help_Start((SocketTextChannel)command.Channel, (SocketGuildUser)command.User);
                 return;
             }
             // For any other condition (if one should exist and not be handled here), create a new menu entry.
@@ -101,7 +101,7 @@ namespace SocialLinker.Commands
             {
                 // Case 4: No previous entry found. Create new entry.
                 // Create a new menu in the current channel.
-                await Help_Menu.Help_Start((SocketTextChannel)Context.Channel, (SocketGuildUser)Context.User);
+                await Help_Menu.Help_Start((SocketTextChannel)command.Channel, (SocketGuildUser)command.User);
                 return;
             }
         }
